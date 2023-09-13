be79f54948dc173ff70b027989727ae340fd29d2
ffniners <codycusimano@yahoo.com>
2023-09-12 22:58:19 -0700

    j

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
-//import 'styles/globals.css';
+
 
 // define our next.js' master MyApp
 function MyApp( { Component, pageProps } ) {
